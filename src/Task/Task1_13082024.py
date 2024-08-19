c30c2ce399c2d8769742c9330937a0dc26bac461
jayasree143 <47857126+jayasree143@users.noreply.github.com>
2024-08-19 12:20:29 +0530

    Update Task1_13082024.py

--- src/Task/Task1_13082024.py
+++ src/Task/Task1_13082024.py
@@ -11,7 +11,18 @@ print(f"{numbers} * 8 = {numbers*8}")
 print(f"{numbers} * 9 = {numbers*9}")
 print(f"{numbers} * 10 = {numbers*10}")
 
+"""
+# Task #1: Multiplication Table
 
+# Get user input
+num = int(input("Enter a number to print its multiplication table: "))
+
+# Print the multiplication table
+print(f"Multiplication Table for {num}:\n")
+for i in range(1, 11):
+    result = num * i
+    print(f"{num} x {i} = {result}")
+"""
 
 
 
