d0df91c35b14a0bd3f4a2fd594d5700bd5228781
jayasree143 <jayasree.251099@gmail.com>
2024-08-20 01:43:20 +0530

    update the task and files

--- src/Task/Task6_18082024.py
+++ src/Task/Task6_18082024.py
@@ -1,5 +1,9 @@
 """
 Task : 6
 Develop a Python script that calculates the square and cube of a given number.
-
 """
+number = int(input("Enter the value for find the given number is cube or square:\n"))
+print(f"Calculating the square of give number:{number * 2}")
+print(f"Calculating the square of give number:{number ** 3}")
+
+
